4419d5a70f485b8b66e298ea06be4087916a33b9
ayada1997 <arpiit12071997@gmail.com>
2022-04-14 22:46:59 +0530

    Prime Factor of number

--- src/MathematicsOperations/PrimeFactors.java
+++ src/MathematicsOperations/PrimeFactors.java
@@ -3,6 +3,7 @@ package MathematicsOperations;
 public class PrimeFactors {
 
 	public static void primeFactors(int n) {
+		if(n<=1) return;
 		for(int i =2;i<n;i++) {
 			if(PrimeNumber.isPrime1(i)) {
 				int x=i;
@@ -12,6 +13,33 @@ public class PrimeFactors {
 				}
 			}
 		}
+	}
+	
+	//Efficient approach
+	
+	public static void primeFactors1(int n) {
+		
+		if(n<=1) return;
+		while(n%2==0) {
+			System.out.println(2);
+			n/=2;	
+		}
+		while(n%3==0) {
+			System.out.println(3);
+			n/=3;	
+		}
+		for(int i=5;i*i<=n;i=+6) {
+			while(n%i==0) {
+				System.out.println(i);
+				n/=i;	
+			}
+			while(n%(i+2)==0) {
+				System.out.println((i+2));
+				n/=(i+2);	
+			}
+		}
+		if(n>3)
+			System.out.println(n);
 	}
 	
 }
